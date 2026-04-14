911c3f4da9c12b233bc0ae9e38b3cc70f67b8d8e
kapiljangid8290 <kapiljangidcds@gmail.com>
2026-04-14 16:37:49 +0530

    removed sanity

--- sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { artistType } from './artistType'
-import { eventType } from './eventType'
-import { venueType } from './venueType'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [artistType, eventType, venueType],
-}
